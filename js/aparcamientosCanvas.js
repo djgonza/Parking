c8f205339545c067588518ef22422350caa93d86
djgonza <davidjimenezgonzalez@MacBook-Pro-de-David.local>
2016-11-03 19:31:25 +0100

    Update

---
+++ js/aparcamientosCanvas.js
@@ -0,0 +1,9 @@
+$(function() {
+
+	var canvas = document.getElementById('aparcamientos');
+	var contexto = canvas.getContext('2d');
+	
+	contexto.fillStyle = '#CCCCCC';
+	contexto.fillRect (0, 0, 450, 350);
+
+});
